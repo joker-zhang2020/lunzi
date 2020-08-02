d438d314c47850b3f5e1708de437a00ae17d10cf
张某某 <youqiRagingStorm@user.noreply.gitee.com>
2020-08-03 07:51:31 +0800

    检测子元素是不是button

--- src/button-group.vue
+++ src/button-group.vue
@@ -5,7 +5,15 @@
 </template>
 
 <script>
-
+    export default {
+        mounted() {
+            for(node of this.$el.children){
+                if(node.nodeName.toLowerCase() !== 'button'){
+                    console.warn(`g-button-group 的自元素应该全是 g-button ，但是你写的是 ${name}`)
+                }
+            }
+        }
+    }
 </script>
 
 <style lang="scss" scoped>
