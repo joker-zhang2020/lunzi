22fcb1cdebfa194f6c42c85e58eec766ae810581
张某某 <youqiRagingStorm@user.noreply.gitee.com>
2020-08-09 23:00:55 +0800

    重构 row 和 col

--- src/row.vue
+++ src/row.vue
@@ -1,31 +1,28 @@
 <template>
-    <div class="row" :style="{marginLeft: -gutter/2+'px', marginRight: -gutter/2+'px'}">
+    <div class="row" :style="rowStyle">
         <slot></slot>
     </div>
 </template>
 <script>
     export default {
-        name: 'GuluRow',
+        name: 'lunziRow',
         props: {
             gutter: {
                 type: [Number, String]
             }
         },
-        created () {
-            console.log('row created')
+        computed: {
+            rowStyle() {
+                let {gutter} = this
+                return {marginLeft: -gutter / 2 + 'px', marginRight: -gutter / 2 + 'px'}
+            }
         },
         mounted () {
-            console.log('row mounted')
-            console.log(this.$children)
             this.$children.forEach((vm) => {
                 vm.gutter = this.gutter
             })
         }
     }
-    var div = document.createElement('div') // created
-    var childDiv = document.createElement('div') // child created
-    div.appendChild(childDiv) // child mounted
-    document.body.appendChild(div) // mounted
 </script>
 <style scoped lang="scss">
     .row{
