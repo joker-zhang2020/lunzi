fcb33267656a4bc57d1e71e830b21fa14e5f94e7
张某某 <youqiRagingStorm@user.noreply.gitee.com>
2020-08-09 20:20:43 +0800

    添加 input 的测试用例

--- src/input.vue
+++ src/input.vue
@@ -1,10 +1,16 @@
 <template>
     <div class="wrapper" :class="{error}">
-            <input :value="value" type="text" :disabled="disabled" :readonly="readonly" />
-        <template v-if="error">
-            <icon name="error" class="icon-error"></icon>
-            <span class="errorMessage">{{error}}</span>
-        </template>
+            <input :value="value" type="text" :disabled="disabled" :readonly="readonly"
+               @change="$emit('change', $event)"
+               @input="$emit('input', $event)"
+               @focus="$emit('focus', $event)"
+               @blur="$emit('blur', $event)"
+        >
+            >
+            <template v-if="error">
+                <icon name="error" class="icon-error"></icon>
+                <span class="errorMessage">{{error}}</span>
+            </template>
     </div>
 </template>
 
