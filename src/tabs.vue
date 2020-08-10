8bbecb97306e387bd9eb569cf6f8c3bf34b44bea
张某某 <youqiRagingStorm@user.noreply.gitee.com>
2020-08-10 16:55:47 +0800

    添加tabs组件并实现切换功能

---
+++ src/tabs.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="tabs">
+        <slot></slot>
+    </div>
+</template>
+<script>
+    import Vue from 'vue'
+    export default {
+        name: 'GuluTabs',
+        props: {
+            selected: {
+                type: String,
+                required: true
+            },
+            direction: {
+                type: String,
+                default: 'horizontal',
+                validator (value) {
+                    return ['horizontal', 'vertical'].indexOf(value) >= 0
+                }
+            }
+        },
+        data () {
+            return {
+                eventBus: new Vue()
+            }
+        },
+        provide () {
+            return {
+                eventBus: this.eventBus
+            }
+        },
+        mounted () {
+            // this.$emit('update:selected', '这是 this $emit 出来的数据')
+            this.eventBus.$emit('update:selected', this.selected)
+            // this.$emit('update:selected', 'xxx')
+        }
+    }
+</script>
+<style>
+    .tabs {
+    }
+</style>
--- test/toast.test.js
+++ test/toast.test.js
@@ -12,7 +12,7 @@ describe('Toast', () => {
     })
 
     describe('props', function () {
-        it('接受 autoClose', (done) => {
+        it('接受 closeButton', (done) => {
             let div = document.createElement('div')
             document.body.appendChild(div)
             const Constructor = Vue.extend(Toast)
@@ -39,8 +39,11 @@ describe('Toast', () => {
             }).$mount()
             let closeButton = vm.$el.querySelector('.close')
             expect(closeButton.textContent.trim()).to.eq('关闭吧')
-            closeButton.click()
-            expect(callback).to.have.been.called
+            setTimeout(()=>{
+                closeButton.click()
+                expect(callback).to.have.been.called
+                done()
+            },200)
         })
         it('接受 enableHtml', () => {
             const Constructor = Vue.extend(Toast)
