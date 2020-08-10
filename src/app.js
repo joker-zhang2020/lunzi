b30e0e6024c1eeac5aadc28a00fbe7bd6d188729
张某某 <youqiRagingStorm@user.noreply.gitee.com>
2020-08-10 15:50:05 +0800

    实现 autoClose closeButton enableHtml多行文字功能

--- src/app.js
+++ src/app.js
@@ -27,6 +27,10 @@ Vue.component('g-sider', Sider)
 Vue.component('g-toast', Toast)
 Vue.use(plugin)
 
+import createElement from 'vue'
+
+const h = createElement
+
 
 new Vue({
     el:'#app',
@@ -36,10 +40,13 @@ new Vue({
         loading3:false,
         message: 'hi'
     },
-    created(){},
+    created(){
+        this.$toast('文字', {
+            enableHtml: false
+        })
+    },
     methods: {
         showToast() {
-            this.$toast('我是 message')
         }
     }
 })
