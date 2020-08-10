e2c3baf405bff32f43ac471e183a77cc2a55efe6
张某某 <youqiRagingStorm@user.noreply.gitee.com>
2020-08-10 17:12:50 +0800

    添加测试用例

--- src/tabs-item.vue
+++ src/tabs-item.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="tabs-item" @click="xxx" :class="classes">
+    <div class="tabs-item" @click="onClick" :class="classes" :data-name="name">
         <slot></slot>
     </div>
 </template>
@@ -25,24 +25,30 @@
         computed: {
             classes () {
                 return {
-                    active: this.active
+                    active: this.active,
+                    disabled: this.disabled
                 }
             }
         },
         created () {
-            this.eventBus.$on('update:selected', (name) => {
-                this.active = name === this.name;
-            })
+            if (this.eventBus) {
+                this.eventBus.$on('update:selected', (name) => {
+                    this.active = name === this.name;
+                })
+            }
         },
         methods: {
-            xxx () {
-                this.eventBus.$emit('update:selected', this.name, this)
+            onClick () {
+                if (this.disabled) { return }
+                this.eventBus && this.eventBus.$emit('update:selected', this.name, this)
+                this.$emit('click', this)
             }
         }
     }
 </script>
 <style lang="scss" scoped>
     $blue: blue;
+    $disabled-text-color: grey;
     .tabs-item {
         flex-shrink: 0;
         padding: 0 1em;
@@ -53,6 +59,10 @@
         &.active {
             color: $blue;
             font-weight: bold;
+        }
+        &.disabled {
+            color: $disabled-text-color;
+            cursor: not-allowed;
         }
     }
 </style>
